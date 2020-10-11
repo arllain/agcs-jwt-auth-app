91641240f58c92a8491fd8a0234894d042b1f33f
Arllain <arllain@gmail.com>
2020-10-11 18:38:12 -0300

    created project

---
+++ src/main/java/com/arllain/agcsjwtauthapp/AgcsJwtAuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.arllain.agcsjwtauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgcsJwtAuthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgcsJwtAuthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arllain/agcsjwtauthapp/AgcsJwtAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arllain.agcsjwtauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgcsJwtAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
